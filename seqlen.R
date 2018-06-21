36b9a706d32029ff79e527c4fe0ef47760c0204d
Sergio Castillo Lara <s.cast.lara@gmail.com>
2018-06-21 16:21:33 +0200

    Now works with new version of ggplot, pls ggplot stop.

--- seqlen.R
+++ seqlen.R
@@ -1,6 +1,7 @@
 #!/usr/bin/R
 library(ggplot2);
 library(scales);
+library(methods);
 
 arguments <- commandArgs(TRUE);
 TRLEN     <- arguments[1];
@@ -33,19 +34,34 @@ miny <- min(merged$PROTEIN)
 mintotal <- min(c(minx, miny)) - 0.05*min(c(minx, miny))
 
 # Plot the thing
-plot <- ggplot(merged, aes(x=TRANSCRIPT, y=PROTEIN)) +
-    geom_point(alpha = 0.5, color="#EBD170") +
-    xlab("\nTranscript Length (number of codons)") +
-    ylab("Protein Length (number of aa)\n") +
-    stat_density2d(color="#002640", alpha=0.5) +
-    scale_x_log10(breaks=breaks2, limits=c(mintotal,xlabel)) + scale_y_log10(breaks=breaks2, limits=c(mintotal,xlabel)) +
-    stat_function(fun=function(x)x+log10(1),    geom="line", colour="#4d4d4d", alpha=0.7, linetype="dashed") +
-    stat_function(fun=function(x)x+log10(0.75), geom="line", colour="#4d4d4d", alpha=0.7, linetype="dashed") +
-    stat_function(fun=function(x)x+log10(0.5),  geom="line", colour="#4d4d4d", alpha=0.7, linetype="dashed") +
-    stat_function(fun=function(x)x+log10(0.25), geom="line", colour="#4d4d4d", alpha=0.7, linetype="dashed") +
-    stat_function(fun=function(x)x+log10(0.1),  geom="line", colour="#4d4d4d", alpha=0.7, linetype="dashed") +
-    annotate("text", label=c("100%","75%","50%","25%", "10%"), x=xlabel, y=c(y100, y75, y50, y25, y10),
-             angle=45, hjust=c(1.2,1.2,1.2,1.2,1.2), vjust=c(1.1,1.1,1.1,1.1,1.1), size=3.5) +
-    theme_bw()
+scatt <- ggplot(merged, aes(x=TRANSCRIPT, y=PROTEIN) ) +
+                geom_point(alpha = 0.3, color="#F7B043") +
+                coord_fixed() +
+                xlab("\nTranscript Lenght (bp)") +
+                ylab("\nLongest-ORF Length (bp)\n") +
+                theme_bw() +
+                theme(legend.position = "none",
+                      plot.title=element_text(vjust=3)) +
+                stat_density2d(color="#002640") +
+                scale_x_log10(breaks=breaks2, limits=c(mintotal, xlabel)) +
+                scale_y_log10(breaks=breaks2, limits=c(mintotal, xlabel)) +
+                stat_function(fun=function(x) log10(x),
+                              geom="line", colour="#4d4d4d", alpha=0.7, linetype="dashed") +
+                stat_function(fun=function(x) log10(x * 0.75),
+                              geom="line", colour="#4d4d4d", alpha=0.7, linetype="dashed") +
+                stat_function(fun=function(x) log10(x * 0.5),
+                              geom="line", colour="#4d4d4d", alpha=0.7, linetype="dashed") +
+                stat_function(fun=function(x) log10(x * 0.25),
+                              geom="line", colour="#4d4d4d", alpha=0.7, linetype="dashed") +
+                stat_function(fun=function(x) log10(x* 0.1),
+                              geom="line", colour="#4d4d4d", alpha=0.7, linetype="dashed") +
+                annotate("text",
+                         label=c("100%","75%","50%","25%", "10%"),
+                         x=xlabel,
+                         y=c(y100, y75, y50, y25, y10),
+                         angle=45,
+                         hjust=c( 0.9, 1.2, 1.2, 1.2, 1.2),
+                         vjust=c(-0.1, 1.1, 1.1, 1.1, 1.1),
+                         size=3.5)
 
-ggsave(file=OUTPUT, plot);
+ggsave(file=OUTPUT, scatt);
